afa01b3ee1481ced12bfa434d24d10eba7616e04
Akerke <akerke.kass@gmail.com>
2018-09-01 18:52:27 +0600

    folder hierarchy renames and restructured

--- products/app.py
+++ products/app.py
@@ -3,15 +3,26 @@
 # ============ #
 from flask import Blueprint, render_template
 
+from core.database import init_db, db_session
+from products.models import Product
+
 
 products_app = Blueprint("products_app", __name__,
                          template_folder="./templates",
                          static_folder="./static",
                          static_url_path='products/static')
+init_db()
 
 
 @products_app.route("/", methods=["GET"])
 def show_products():
-    return "need to render products"
+    products = Product.query.all()
+    template_path = "products/products.html"
+    context = {
+        "products": products,
+        "products_page": True,
+
+    }
+    return render_template(template_path, **context)
 
 
---
+++ products/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String
+from core.database import Base
+
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(50))
+    price = Column(Integer)
+    description = Column(String(120))
+    img_path = Column(String(120))
+
+    def __init__(self, title=None, price=None, description=None, img_path=None):
+        self.title = title
+        self.price = price
+        self.description = description
+        self.img_path = img_path
+
+    def __repr__(self):
+        return '<Product %r>' % self.title
+
